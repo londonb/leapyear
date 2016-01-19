6b68988ba58dd1b36bc69cf5e659c789c4c73792
Brad London and Jess Guilford <brad.london@gmail.com>
2016-01-19 15:40:11 -0800

    add index

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 var leapYear = function(year) {
-  if (year % 100 === 0){
-    return false;
-  } else if (year % 4 === 0) {
-    return true;
-  } else {
-  return false;
-  }
+  return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0);
 };
+
+$(document).ready(function(){
+  $("form#leap-year").submit(function(event){
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (isNaN(year)) {
+    alert("that's not a number");
+    } else if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,6 @@
 describe('leapYear', function() {
   it("is false for a year that is not divisible by 4, 100, or 400", function() {
-    expect(leapYear(2001)).to.equal(false);
+    expect(leapYear(1993)).to.equal(false);
   });
 
   it("is true for years divisible by 4", function() {
@@ -9,5 +9,13 @@ describe('leapYear', function() {
 
   it("is false for years divisible by 100", function(){
     expect(leapYear(1900)).to.equal(false);
+  });
+
+  it("is true for years divisible by 400", function() {
+    expect(leapYear(2000)).to.equal(true);
+  });
+
+  it("is true for years that are a number", function() {
+    expect(leapYear("hi")).to.equal(false);
   });
 });
